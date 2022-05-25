228f178c7c11501b8cbcc47878519af4e41dbd9d
Radek Šmejkal <radeksmejky9@gmail.com>
2022-05-25 10:14:12 +0200

    feat: profile page

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,3 +1,13 @@
 <template>
   <div>PROFILE PAGE</div>
+  <div class="grid grid-cols-6 gap-10">
+    <div class="col-start-1 col-end-7 h-64 bg-orange-400 ...">background</div>
+    <div class="col-start-1 col-end-1 square w-32 h-32 bg-orange-400 ...">
+      img
+    </div>
+    <div class="col-start-2 col-end-4 bg-orange-400 text-center ...">
+      Rakek Šmejkal
+    </div>
+    <div class="col-start-1 col-end-7 bg-orange-400 ...">04</div>
+  </div>
 </template>
